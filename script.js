c8a34abdf4cd1ecdebeb56a9a5c1a85ecc8449f5
Saad Sahir <133065206+saad-sahir@users.noreply.github.com>
2024-09-12 21:29:14 +0530

    test features added

--- script.js
+++ script.js
@@ -43,3 +43,42 @@ function sendMessage() {
         })
     }
 }
+
+function testAPI() {
+    const userInput = document.getElementById('user-input').value;
+
+    if (userInput.trim() !== "") {
+        const chatBox = document.getElementById('chat-box');
+
+        const userMessageDiv = document.createElement('div');
+        userMessageDiv.classList.add('message');
+        userMessageDiv.textContent = userInput;
+
+        chatBox.appendChild(userMessageDiv);
+
+        document.getElementById('user-input').value = '';
+
+        fetch('https://test-api-etok.onrender.com/test', {
+            method:"POST",
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify({query: userInput})
+        })
+        .then(response => response.json())
+        .then(data =>{
+            const botMessageDiv = document.createElement('div');
+            botMessageDiv.classList.add('bot_message');
+            botMessageDiv.innerHTML = marked.parse(data.response);
+            chatBox.appendChild(botMessageDiv)
+            chatBox.scrollTop = chatBox.scrollHeight;
+        })
+        .catch(error => {
+            console.error("Error: ", error);
+            const errorMessageDiv = document.createElement('div');
+            errorMessageDiv.classList.add('message');
+            errorMessageDiv.textContent = "Error: Couldn't connect to the server.";
+            chatBox.appendChild(errorMessageDiv);
+        })
+    }
+}
